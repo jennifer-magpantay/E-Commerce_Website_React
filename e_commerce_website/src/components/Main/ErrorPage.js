87b3f193b4b34cec9c41e7c5c7b340da216af585
jennifer-magpantay <jennifer.magpantay@gmail.com>
2020-09-15 14:35:04 +0100

    Hosting project into Firebase

--- e_commerce_website/src/components/Main/ErrorPage.js
+++ e_commerce_website/src/components/Main/ErrorPage.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React from "react";
+import SignFooter from "../SignIn/SignFooter";
+import SignHeader from "../SignIn/SignHeader";
+import styled from 'styled-components';
+import { Link } from "react-router-dom";
+
+const ContentContainer = styled.div`
+width: 70%;
+height: calc(100vh - 100px - 300px);
+margin: 8% auto;
+text-align: center;
+& h1{
+    font-size: 60px;
+    margin: 0 0 2% 0;
+}
+& h3 {
+    font-size: 28px;
+    margin: 0;
+}
+`;
 
 function ErrorPage() {
-    return (
-        <div>
-            
-        </div>
-    )
+   return (
+    <>
+      <SignHeader />
+           <ContentContainer>
+          <h1>It seems that something wrong happens</h1>
+          <h3>The page you are trying to reach is not available</h3>
+          <p>To go back to Home, click <strong><Link to="/home"> here</Link></strong></p>
+      </ContentContainer>
+           <SignFooter />
+      </>
+  );
 }
 
 export default ErrorPage;
-
-
-
